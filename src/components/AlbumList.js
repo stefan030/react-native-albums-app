d0749415354652bb8e5bc3d9a90e7269e433a780
Stefan <stefanbogdanovic030@gmail.com>
2018-09-05 22:59:11 +0200

    Work with Image tags, scrollable content, user input handle, linking between app and web browser

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View} from 'react-native';
+import {View, ScrollView} from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -20,11 +20,10 @@ export default class Albums extends Component {
     }
 
     render() {
-        console.log(this.state);
         return(
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         )
     }
 }
